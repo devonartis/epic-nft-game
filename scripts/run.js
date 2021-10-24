1ed8cbd22cb97557fd7485d90303423313ffbeee
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-10-24 14:59:38 -0400

    Revert "Updated contract and run.js"
    This reverts commit cac8b4ed8d62a6d68b05755a25efe28a734c3a9c.

--- scripts/run.js
+++ scripts/run.js
@@ -15,15 +15,11 @@ const main = async () => {
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
-let txn;
+  let txn;
 // We only have three characters.
 // an NFT w/ the character at index 2 of our array.
 txn = await gameContract.mintCharacterNFT(2);
 await txn.wait();
-
-// Transfer to attack the boss
-//txn = await gameContract.attackBoss();
-//await txn.wait();
 
 // Get the value of the NFT's URI.
 let returnedTokenUri = await gameContract.tokenURI(1);
