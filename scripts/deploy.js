2b27af4f834718ff229c7f8760dc3caef015f2f7
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-10-24 11:18:56 -0400

    Add hardhard.config.js from being committed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,8 +14,23 @@ const main = async () => {
   let txn;
 // We only have three characters.
 // an NFT w/ the character at index 2 of our array.
+txn = await gameContract.mintCharacterNFT(0);
+await txn.wait();
+console.log("Minted NFT #1");
+
+
+txn = await gameContract.mintCharacterNFT(1);
+await txn.wait();
+console.log("Minted NFT #2");
+
 txn = await gameContract.mintCharacterNFT(2);
 await txn.wait();
+console.log("Minted NFT #3");
+
+txn = await gameContract.mintCharacterNFT(3);
+await txn.wait();
+console.log("Minted NFT #4");
+
 
 // Get the value of the NFT's URI.
 let returnedTokenUri = await gameContract.tokenURI(1);
